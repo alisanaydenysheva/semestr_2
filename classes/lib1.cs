7485cc37ba9ccc96653175f7f6339376e23a126e
alisanaydenysheva <93647369+alisanaydenysheva@users.noreply.github.com>
2022-05-13 12:20:49 +0300

    Create lib1.cs

---
+++ classes/lib1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ClassLibraryTEST
+{
+    public class Class1
+    {
+        static public void test_function()
+        {
+            Console.WriteLine("Проверка");
+        }
+    }
+}
